7a756cea15353b1fa9e121609424423395a0db04
Raloooo <optimuslove0223@icloud.com>
2022-04-06 00:47:34 +0900

    only commit..20220406 12:47

--- cal.sh
+++ cal.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+arr = ('cat num1.txt','cat num2.txt') 
+value1=${arr[0]}
+value2=${arr[1]}
 
-value1=$(<num1.txt)
-value2=$(<num2.txt)
+
+
+
+
 
 if [ -z "$1" ]; then
 	echo "...none operator parameter..."
@@ -11,6 +16,7 @@ if [ -z "$1" ]; then
 	do
 	
 		echo ""
+
 		echo "num1 : " $value1
 		echo "num2 : " $value2
 	
